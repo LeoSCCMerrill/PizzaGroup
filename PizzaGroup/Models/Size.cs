f0698c9ad5ec60c16ba8d4753e13031ddcbb0336
Leo Merrill <lm755387@southeast.edu>
2023-10-11 13:58:34 -0500

    Added a bunch of default values to the database application context

--- PizzaGroup/Models/Size.cs
+++ PizzaGroup/Models/Size.cs
@@ -4,6 +4,7 @@
     {
         public int SizeId { get; set; }
         public string SizeName { get; set; }
+        public int SizeInches { get; set; }
         public Decimal SizePriceMultiplier { get; set; }
     }
 }
