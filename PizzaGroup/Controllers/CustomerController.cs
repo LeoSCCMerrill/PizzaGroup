173f02707a44a3a67c8aa3d28cdd949532b3e20a
John Ramirez <jr704134@southeast.edu>
2023-10-11 14:48:49 -0500

    fixed asp-for CustomerID on Order index page

--- PizzaGroup/Controllers/CustomerController.cs
+++ PizzaGroup/Controllers/CustomerController.cs
@@ -29,7 +29,7 @@ namespace PizzaGroup.Controllers
             return View();
         }
 
-        [Authorize(Roles="Manager")]
+        [Authorize(Roles = "Manager")]
         [HttpPost]
         public IActionResult DeletePizza(int PizzaID)
         {
