665715308a859e1db0e2af00b79acfc9ef4ade6e
MadyWatanabe <99287402+MadyWatanabe@users.noreply.github.com>
2023-11-01 14:04:37 -0500

    Edit function is updated and working. Don't forget to update your Database before running!

---
+++ PizzaGroup/Migrations/20231101190109_Initial.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaGroup.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "22d6208e-e968-487e-a8f6-59a1c3ce94d7",
+                column: "ConcurrencyStamp",
+                value: "9f081fe2-a007-41e8-ab4e-57237a5cd8a2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5cb99a62-bceb-4b4a-98d7-b250d8d7ae11",
+                column: "ConcurrencyStamp",
+                value: "04120ce7-2146-427f-9df9-65cc3284c9d7");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e73",
+                column: "ConcurrencyStamp",
+                value: "8078838d-2c9c-4a4b-a310-f81cfa2e060a");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "22d6208e-e968-487e-a8f6-59a1c3ce94d7",
+                column: "ConcurrencyStamp",
+                value: "0d497156-09fa-422c-9946-b574f3e4cc87");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "5cb99a62-bceb-4b4a-98d7-b250d8d7ae11",
+                column: "ConcurrencyStamp",
+                value: "19bc488e-92f4-4fe6-a8db-ac559f398834");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b4280b6a-0613-4cbd-a9e6-f1701e926e73",
+                column: "ConcurrencyStamp",
+                value: "626523b0-9077-45f8-bef7-2f6a90bdb35a");
+        }
+    }
+}
