9ac8cf5bffc27877322f2bfca63fd64fc9d6b576
Ryan Zimmermann <rz738002@southeast.edu>
2023-11-27 13:09:39 -0600

    Commit

--- PizzaGroup/Controllers/EmployeeController.cs
+++ PizzaGroup/Controllers/EmployeeController.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis;
 using Microsoft.EntityFrameworkCore;
 using PizzaGroup.Data;
 using PizzaGroup.Models;
+using System.Reflection.Metadata;
 using System.Security.Claims;
 
 namespace PizzaGroup.Controllers
@@ -21,8 +24,8 @@ namespace PizzaGroup.Controllers
             IList<Order> orders = _context.Orders.Include(o => o.OrderPizza).ThenInclude(op => op.Pizza).ThenInclude(p => p.Crust)
                                                  .Include(o => o.OrderPizza).ThenInclude(op => op.Pizza).ThenInclude(p => p.Size)
                                                  .Include(o => o.OrderPizza).ThenInclude(op => op.Pizza).ThenInclude(p => p.PizzaToppings)
-                                                 .ThenInclude(pt => pt.Topping)
-                                                 .Where(o => o.EmployeeId.Equals(User.FindFirstValue(ClaimTypes.NameIdentifier)) && o.OrderStatus != OrderStatus.DELIVERED).ToList();
+                                                 .ThenInclude(pt => pt.Topping).ToList();
+                                                 //.Where(o => o.EmployeeId.Equals(User.FindFirstValue(ClaimTypes.NameIdentifier)) && o.OrderStatus != OrderStatus.DELIVERED).ToList();
             return View(orders);
         }
         [HttpGet]
@@ -40,20 +43,22 @@ namespace PizzaGroup.Controllers
             return View(order);
         }
         [HttpPost]
-        public IActionResult UpdateStatus(Order order, int status)
+        public IActionResult UpdateStatus(int orderId)
         {
+            
+            
             //Temp to see what is easier seding the Order ID or the whole Order
-            //Order order = _context.Orders.Find(orderId);
+            Order? order = _context.Orders.Find(orderId);
 
             if (order == null)
             {
                 return NotFound();
             }
 
-            order.OrderStatus = (OrderStatus)status;
-            _context.Orders.Update(order);
-            _context.SaveChanges();
-            return RedirectToAction("Details", order);
+            ////order.OrderStatus = (OrderStatus)status;
+            //_context.Orders.Update(order);
+            //_context.SaveChanges();
+            return RedirectToAction("Index");
         }
         [HttpGet]
         public IActionResult DeleteOrder(int id)
@@ -100,5 +105,6 @@ namespace PizzaGroup.Controllers
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+        
     }
 }
